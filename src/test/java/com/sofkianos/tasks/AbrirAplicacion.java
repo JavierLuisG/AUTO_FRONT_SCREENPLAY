222b24e652d991aa1727fc1c6de467c9392dcbfb
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2026-03-13 00:25:03 -0500

    feat: create AbrirNavegador class for browser opening functionality

--- src/test/java/com/sofkianos/tasks/AbrirAplicacion.java
+++ src/test/java/com/sofkianos/tasks/AbrirAplicacion.java
@@ -2,13 +2,13 @@ package com.sofkianos.tasks;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
+import com.sofkianos.hooks.AbrirNavegador;
 import com.sofkianos.util.Constantes;
 import com.sofkianos.ui.PaginaInicio;
 import net.serenitybdd.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Open;
 
 public class AbrirAplicacion implements Task {
 
@@ -20,7 +20,7 @@ public class AbrirAplicacion implements Task {
     @Step("abrir la aplicacion de reconocimientos")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Open.url(Constantes.URL),
+                AbrirNavegador.abrirUrl(Constantes.URL),
                 Click.on(PaginaInicio.BOTON_ACCEDER)
         );
     }
